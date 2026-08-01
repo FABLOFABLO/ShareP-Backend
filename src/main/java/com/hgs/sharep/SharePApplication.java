401c468818649145d495262425f3256c1ec532dd
gkswltjr0725 <rkdgpdud04@gmail.com>
2026-08-01 22:45:43 +0900

    first commit

---
+++ src/main/java/com/hgs/sharep/SharePApplication.java
@@ -0,0 +1,13 @@
+package com.hgs.sharep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharePApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SharePApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hgs/sharep/SharePApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hgs.sharep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharePApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
