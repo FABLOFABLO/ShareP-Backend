d7fd87bcec6db1c8e41bd0e554743043455ad9e0
kimiwoo0823 <kimiwoo0823@gmail.com>
2026-09-16 18:48:43 +0900

    fix: 팔로워 조회랑 팔로잉 조회 반대로 되어있던거 수정

--- src/main/java/com/sharep/domain/follow/service/FollowingReadService.java
+++ src/main/java/com/sharep/domain/follow/service/FollowingReadService.java
@@ -5,7 +5,7 @@ import com.sharep.domain.follow.domain.repository.FollowRepository;
 import com.sharep.domain.follow.presentation.dto.response.FollowerResponse;
 import com.sharep.domain.user.domain.User;
 import com.sharep.domain.user.domain.repository.UserRepository;
-import com.sharep.domain.follow.presentation.dto.response.FollowingResponse;
+import com.sharep.domain.follow.presentation.dto.response.FollowerResponse;
 import com.sharep.global.error.exception.CustomException;
 import com.sharep.global.error.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
@@ -21,27 +21,27 @@ public class FollowingReadService {
     private final UserRepository userRepository;
 
     @Transactional(readOnly = true)
-    public List<FollowingResponse> execute(Long userId, Long currentId) {
+    public List<FollowerResponse> execute(Long userId, Long currentId) {
         if (userRepository.findById(userId).isEmpty()) {
             throw new CustomException(ErrorCode.USERID_NOT_FOUND);
         }
 
-        List<Follow> follows = followRepository.findAllByFollowing(userRepository.findById(userId)
+        List<Follow> follows = followRepository.findAllByFollower(userRepository.findById(userId)
                 .orElseThrow(() -> new CustomException(ErrorCode.FOLLOWING_NOT_FOUND)));
 
-        List<FollowingResponse> followingResponse = follows.stream()
+        List<FollowerResponse> followerResponse = follows.stream()
                 .map(follow -> {
                     User currentUser = userRepository.findById(currentId)
                             .orElseThrow(() -> new CustomException(ErrorCode.USERID_NOT_FOUND));
-                    if (followRepository.findByFollowerAndFollowing(currentUser, follow.getFollowing()) != null) {
-                        return new FollowingResponse(follow, true);
+                    if (followRepository.findByFollowerAndFollowing(currentUser, follow.getFollower()) != null) {
+                        return new FollowerResponse(follow, true);
                     }
                     else {
-                        return new FollowingResponse(follow, false);
+                        return new FollowerResponse(follow, false);
                     }
                 })
                 .toList();
 
-        return followingResponse;
+        return followerResponse;
     }
 }
